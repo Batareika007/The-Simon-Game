a7828f940e28d2578bb97416a875c9cd81cf3833
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-05-13 02:07:20 +0300

    starting game script

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let buttonColours = ['red', 'blue', 'green', 'yellow'];
+
+let gamePattern = [];
+
+
+
+// add to gamePattern random color
+function nextSequence(){
+    
+    let randomNumber = Math.floor(Math.random()*4);
+  
+    let randomChosenColour = buttonColours[randomNumber];
+    
+    gamePattern.push(randomChosenColour);
+
+    $('#'+randomChosenColour).fadeOut(250).fadeIn(50);
+    new Audio('../sounds/' + randomChosenColour + '.mp3').play();
+}
+
+// run function on any keypress
+$(document).keypress(nextSequence);
