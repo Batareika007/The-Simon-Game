136f97fe13ea545c7536f4a9e0b261b88787ec93
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-05-14 12:37:53 +0300

    script editing

--- js/script.js
+++ js/script.js
@@ -3,19 +3,36 @@ let buttonColours = ['red', 'blue', 'green', 'yellow'];
 let gamePattern = [];
 
 
+let randomNumber = Math.floor(Math.random() * 4);
+
+let randomChosenColour = buttonColours[randomNumber];
 
 // add to gamePattern random color
-function nextSequence(){
-    
-    let randomNumber = Math.floor(Math.random()*4);
-  
+function nextSequence() {
+
+    let randomNumber = Math.floor(Math.random() * 4);
+
     let randomChosenColour = buttonColours[randomNumber];
-    
+
     gamePattern.push(randomChosenColour);
 
-    $('#'+randomChosenColour).fadeOut(250).fadeIn(50);
+    // $('#' + randomChosenColour).fadeOut(250).fadeIn(50);
+    $('#' + randomChosenColour).addClass(randomChosenColour + "-active").delay(1000).removeClass(randomChosenColour + "-active");
     new Audio('sounds/' + randomChosenColour + '.mp3').play();
 }
 
 // run function on any keypress
 $(document).keypress(nextSequence);
+
+$(".red").hover(function () {
+    $(this).toggleClass("red-active");
+});
+$(".green").hover(function () {
+    $(this).toggleClass("green-active");
+});
+$(".yellow").hover(function () {
+    $(this).toggleClass("yellow-active");
+});
+$(".blue").hover(function () {
+    $(this).toggleClass("blue-active");
+});
