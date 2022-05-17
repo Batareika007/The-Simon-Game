0d0f3d3e799069bfb01f01c6064e798a4bd436ac
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-05-17 22:30:54 +0300

    almost done
    game worked, but has a lot of bugs, need to repair scripts

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let buttonColours = ['red', 'blue', 'green', 'yellow'];
+const buttonColours = ['red', 'blue', 'green', 'yellow'];
 
 let gamePattern = [];
 
@@ -10,56 +10,90 @@ let level = 0;
 
 // add to gamePattern random color
 function nextSequence() {
-    
+    level++
     let randomNumber = Math.floor(Math.random() * 4);
 
     let randomChosenColour = buttonColours[randomNumber];
-    
+
     gamePattern.push(randomChosenColour);
 
+    playSound(randomChosenColour);
     animatedPress(randomChosenColour);
 
-    playSound(randomChosenColour);
 
-    $('h1').text('Level ' + level)
-    
-    console.log(level)
-    
-    level++
+    $('#level-title').text('Level ' + level);
+
+    console.log('level: ' + level);
+    console.log(gamePattern);
+
 }
 
 
 $('.btn').click(function () {
-    let userChosenColour = $(this).attr('id');
-    
-    userClickedPattern.push(userChosenColour);
-    
-    playSound(userChosenColour);
-    
-    animatedPress(userChosenColour)
 
-    console.log(userClickedPattern);
+    if (keyPressed.length > 0) {
+
+        let userChosenColour = $(this).attr('id');
+
+        userClickedPattern.push(userChosenColour);
+
+        playSound(userChosenColour);
+
+        animatedPress(userChosenColour)
+
+        console.log('user clicked: ' + userClickedPattern);
+
+        theGame();
+    }
 });
 
 
-function playSound(name){
+function playSound(name) {
     new Audio('sounds/' + name + '.mp3').play();
 }
 
-function animatedPress(currentColour){
+function animatedPress(currentColour) {
     $('#' + currentColour).addClass(currentColour + "-active").delay(250).queue(function (removeClass) {
         $(this).removeClass(currentColour + "-active");
         removeClass();
     });
 }
 
+// detect any keypress on keyboard
 let keyPressed = [];
-$(document).keydown(function(event){
-    if (keyPressed.length === 0){
+
+// -- start game --- detect keyboard press for first time and call nextSequence()
+
+$(document).keydown(function (event) {
+    if (keyPressed.length === 0) {
         nextSequence();
+        keyPressed.push(event.key);
+        // startTheGame();
     }
-    keyPressed.push(event.key)
 });
 
 
+// ==== the game  ===== // 
 
+function theGame() {
+    if (userClickedPattern[userClickedPattern.length - 1] == (gamePattern[userClickedPattern.length - 1])) {
+        console.log('correct');
+        if (gamePattern.length > userClickedPattern.length) {
+            console.log('click again')
+       
+        } else {
+            setTimeout(function () {
+                nextSequence()
+            }, 1000);
+            userClickedPattern = [];
+        }
+    } else {
+        console.log('game over');
+        userClickedPattern = []
+        gamePattern = [];
+        keyPressed = [];
+        level = 0;
+        $('#level-title').text('Game over!');
+    }
+
+}
